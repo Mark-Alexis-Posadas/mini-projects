80be996118c485ee0c82fcd7f4907c26876db70c
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2023-07-24 09:37:21 +0800

    Added todo in usereducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,9 @@
-import { useState } from "react";
+import Todo from "./components/simple-todo-app/UseReducer/Todo";
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
-  const [currentTodo, setCurrentTodo] = useState("");
-  const [alert, setAlert] = useState(false);
-
-  const handleTodoChange = (e) => {
-    setCurrentTodo(e.target.value);
-  };
-
-  const handleAddTodo = () => {
-    if (currentTodo.trim() !== "") {
-      setTodos([...todos, { text: currentTodo, done: false }]);
-      setCurrentTodo("");
-    } else {
-      setAlert(true);
-    }
-  };
-
-  const handleTodoDone = (index) => {
-    const updatedTodos = [...todos];
-    updatedTodos[index].done = true;
-    setTodos(updatedTodos);
-  };
-
   return (
     <>
-      <div className="mt-5 container">
-        <h1>Simple Todo</h1>
-        <div className="d-flex flex-column flex-sm-row ms-sm-2">
-          <input
-            type="text"
-            placeholder="Enter todo"
-            className="form-control"
-            value={currentTodo}
-            onChange={handleTodoChange}
-          />
-          <button className="mt-3 btn btn-primary" onClick={handleAddTodo}>
-            Add todo
-          </button>
-        </div>
-        {alert && (
-          <p className="alert alert-danger mt-2">Please write your todo.</p>
-        )}
-        <ul className="list-group mt-3 w-100">
-          {todos.map((todo, idx) => (
-            <li
-              className={`list-group-item ${
-                todo.done ? "done" : ""
-              } d-flex align-items-center justify-content-between`}
-              key={idx}
-            >
-              {todo.text}
-              {!todo.done && (
-                <button
-                  className="btn btn-sm btn-outline-danger ms-2"
-                  onClick={() => handleTodoDone(idx)}
-                >
-                  X
-                </button>
-              )}
-            </li>
-          ))}
-        </ul>
-      </div>
+      <Todo />
     </>
   );
 };
---
+++ src/components/simple-todo-app/UseReducer/Todo.jsx
@@ -0,0 +1,86 @@
+import { useState, useReducer } from "react";
+
+const todoReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      if (action.payload.trim() !== "") {
+        return [...state, { text: action.payload, done: false }];
+      }
+      return state;
+    case "MARK_DONE":
+      return state.map((todo, index) =>
+        index === action.payload ? { ...todo, done: true } : todo
+      );
+    default:
+      return state;
+  }
+};
+
+const Todo = () => {
+  const [todos, dispatch] = useReducer(todoReducer, []);
+  const [currentTodo, setCurrentTodo] = useState("");
+  const [alert, setAlert] = useState(false);
+
+  const handleTodoChange = (e) => {
+    setCurrentTodo(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (currentTodo.trim() !== "") {
+      dispatch({ type: "ADD_TODO", payload: currentTodo });
+      setCurrentTodo("");
+    } else {
+      setAlert(true);
+    }
+  };
+
+  const handleTodoDone = (index) => {
+    dispatch({ type: "MARK_DONE", payload: index });
+  };
+
+  return (
+    <div className="mt-5 container">
+      <h1>Simple Todo</h1>
+      <div className="d-flex flex-column flex-sm-row ms-sm-0">
+        <input
+          type="text"
+          placeholder="Enter todo"
+          className="form-control"
+          value={currentTodo}
+          onChange={handleTodoChange}
+        />
+        <button
+          className="mt-3 mt-sm-0 ms-sm-3 btn btn-primary"
+          onClick={handleAddTodo}
+        >
+          Add todo
+        </button>
+      </div>
+      {alert && (
+        <p className="alert alert-danger mt-2">Please write your todo.</p>
+      )}
+      <ul className="list-group mt-3 w-100">
+        {todos.map((todo, idx) => (
+          <li
+            className={`list-group-item ${
+              todo.done ? "done" : ""
+            } d-flex align-items-center justify-content-between`}
+            key={idx}
+          >
+            {todo.text}
+            {!todo.done && (
+              <button
+                className="btn btn-sm btn-outline-danger ms-2"
+                onClick={() => handleTodoDone(idx)}
+              >
+                X
+              </button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Todo;
